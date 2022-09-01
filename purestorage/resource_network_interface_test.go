732a4a9af3f444c1085fee5fa4f71c356a4015cc
Sander Descamps <sander.descamps@ingenico.com>
2022-09-01 16:37:52 +0200

    disable network tests as they are not supported

--- purestorage/resource_network_interface_test.go
+++ purestorage/resource_network_interface_test.go
@@ -16,73 +16,65 @@
 
 package purestorage
 
-import (
-	"fmt"
-	"strings"
-	"testing"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-)
-
 // Configure DNS settings
-func TestAccResourcePureNetworkInterface_create(t *testing.T) {
-	ifname := "vir1"
-	address1 := "192.168.6.3"
-	address2 := "192.168.6.4"
-	gateway := "192.168.6.1"
-	netmask := "255.255.255.0"
-	resource_name := fmt.Sprintf("purefa_network_interface.tfnetworkinterface%stest", strings.Replace(ifname, ".", "_", -1))
+// func TestAccResourcePureNetworkInterface_create(t *testing.T) {
+// 	ifname := "vir1"
+// 	address1 := "192.168.6.3"
+// 	address2 := "192.168.6.4"
+// 	gateway := "192.168.6.1"
+// 	netmask := "255.255.255.0"
+// 	resource_name := fmt.Sprintf("purefa_network_interface.tfnetworkinterface%stest", strings.Replace(ifname, ".", "_", -1))
 
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCheckPureNetworkInterfaceConfig(ifname, address1, gateway, netmask, true, 1500),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resource_name, "ifname", ifname),
-					resource.TestCheckResourceAttr(resource_name, "address", address1),
-					resource.TestCheckResourceAttr(resource_name, "gateway", gateway),
-					resource.TestCheckResourceAttr(resource_name, "netmask", netmask),
-					resource.TestCheckResourceAttr(resource_name, "enabled", "true"),
-					resource.TestCheckResourceAttr(resource_name, "mtu", "1500"),
-				),
-			},
-			{
-				Config: testAccCheckPureNetworkInterfaceConfig(ifname, address1, gateway, netmask, false, 1500),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resource_name, "ifname", ifname),
-					resource.TestCheckResourceAttr(resource_name, "address", address1),
-					resource.TestCheckResourceAttr(resource_name, "gateway", gateway),
-					resource.TestCheckResourceAttr(resource_name, "netmask", netmask),
-					resource.TestCheckResourceAttr(resource_name, "enabled", "false"),
-					resource.TestCheckResourceAttr(resource_name, "mtu", "1500"),
-				),
-			},
-			{
-				Config: testAccCheckPureNetworkInterfaceConfig(ifname, address2, gateway, netmask, true, 9000),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resource_name, "ifname", ifname),
-					resource.TestCheckResourceAttr(resource_name, "address", address2),
-					resource.TestCheckResourceAttr(resource_name, "gateway", gateway),
-					resource.TestCheckResourceAttr(resource_name, "netmask", netmask),
-					resource.TestCheckResourceAttr(resource_name, "enabled", "true"),
-					resource.TestCheckResourceAttr(resource_name, "mtu", "1500"),
-				),
-			},
-		},
-	})
-}
+// 	resource.Test(t, resource.TestCase{
+// 		PreCheck:  func() { testAccPreCheck(t) },
+// 		Providers: testAccProviders,
+// 		Steps: []resource.TestStep{
+// 			{
+// 				Config: testAccCheckPureNetworkInterfaceConfig(ifname, address1, gateway, netmask, true, 1500),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resource_name, "ifname", ifname),
+// 					resource.TestCheckResourceAttr(resource_name, "address", address1),
+// 					resource.TestCheckResourceAttr(resource_name, "gateway", gateway),
+// 					resource.TestCheckResourceAttr(resource_name, "netmask", netmask),
+// 					resource.TestCheckResourceAttr(resource_name, "enabled", "true"),
+// 					resource.TestCheckResourceAttr(resource_name, "mtu", "1500"),
+// 				),
+// 			},
+// 			{
+// 				Config: testAccCheckPureNetworkInterfaceConfig(ifname, address1, gateway, netmask, false, 1500),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resource_name, "ifname", ifname),
+// 					resource.TestCheckResourceAttr(resource_name, "address", address1),
+// 					resource.TestCheckResourceAttr(resource_name, "gateway", gateway),
+// 					resource.TestCheckResourceAttr(resource_name, "netmask", netmask),
+// 					resource.TestCheckResourceAttr(resource_name, "enabled", "false"),
+// 					resource.TestCheckResourceAttr(resource_name, "mtu", "1500"),
+// 				),
+// 			},
+// 			{
+// 				Config: testAccCheckPureNetworkInterfaceConfig(ifname, address2, gateway, netmask, true, 9000),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resource_name, "ifname", ifname),
+// 					resource.TestCheckResourceAttr(resource_name, "address", address2),
+// 					resource.TestCheckResourceAttr(resource_name, "gateway", gateway),
+// 					resource.TestCheckResourceAttr(resource_name, "netmask", netmask),
+// 					resource.TestCheckResourceAttr(resource_name, "enabled", "true"),
+// 					resource.TestCheckResourceAttr(resource_name, "mtu", "1500"),
+// 				),
+// 			},
+// 		},
+// 	})
+// }
 
-func testAccCheckPureNetworkInterfaceConfig(ifname string, address string, gateway string, netmask string, enabled bool, mtu int) string {
-	return fmt.Sprintf(`
-			resource "purefa_network_interface" "tfnetworkinterface%stest" {
-				name = "%s"
-				address = "%s"
-				gateway = "%s"
-				netmask = "%s"
-				enabled = %t
-				mtu = %d
-			}`, strings.Replace(ifname, ".", "_", -1), ifname, address, gateway, netmask, enabled, mtu)
+// func testAccCheckPureNetworkInterfaceConfig(ifname string, address string, gateway string, netmask string, enabled bool, mtu int) string {
+// 	return fmt.Sprintf(`
+// 			resource "purefa_network_interface" "tfnetworkinterface%stest" {
+// 				name = "%s"
+// 				address = "%s"
+// 				gateway = "%s"
+// 				netmask = "%s"
+// 				enabled = %t
+// 				mtu = %d
+// 			}`, strings.Replace(ifname, ".", "_", -1), ifname, address, gateway, netmask, enabled, mtu)
 
-}
+// }
