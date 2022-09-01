ed48aef2c52c5a437bbc68f18555f2778c2be003
Sander Descamps <sander.descamps@ingenico.com>
2022-09-01 13:46:53 +0200

    test-env script

---
+++ scripts/test-env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# yum install git gcc
+# git clone https://github.com/sanderdescamps/terraform-provider-flash.git
+
+wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
+
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+
+
+export PURE_USERNAME=pureuser
+export PURE_PASSWORD=pureuser
+export PURE_TARGET=flasharray1.testdrive.local
+
+TF_ACC=1 go test -run <test_name> -v ./...
